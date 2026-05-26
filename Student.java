69696fdb6eae3f2ad74f557a66d36f7a4180ed5d
sezar712ma-spec <sezar712ma@gmail.com>
2026-05-26 05:12:10 +0000

    oop

---
+++ Student.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Student {
+    public static void main (String[] args ) {
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.print("student_name ");
+        String student_name = input.nextLine();
+       
+       System.out.print("student_id");
+       String student_id = input.nextLine();
+        
+        System.out.print("coding_marks");
+        String coding_marks  = input.nextLine();
+
+    }
+    
+}
